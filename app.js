636018c08749bb13838cf088259106a49a4e2588
uliana-cogniance <ukostur@cogniance.com>
2016-12-23 20:51:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*
+e/*
  * Module dependencies
  */
 
@@ -8,7 +8,7 @@ var express = require('express')
 
 
 var app = express()
-var port = 80
+var port = process.env.PORT
 
 function compile(str, path) {
   return stylus(str)
